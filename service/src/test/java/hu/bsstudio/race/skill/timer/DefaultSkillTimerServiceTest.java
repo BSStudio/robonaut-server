ee46407c880ea223ede4e20fcc19207ef475eb82
csikb <22349790+csikb@users.noreply.github.com>
2020-12-23 08:48:01 +0100

    test skill race services

---
+++ service/src/test/java/hu/bsstudio/race/skill/timer/DefaultSkillTimerServiceTest.java
@@ -0,0 +1,61 @@
+package hu.bsstudio.race.skill.timer;
+
+import hu.bsstudio.common.model.TimerAction;
+import hu.bsstudio.race.skill.timer.model.SkillTimer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+final class DefaultSkillTimerServiceTest {
+
+    private static final int TIMER_AT = 2020;
+
+    private DefaultSkillTimerService underTest;
+
+    @BeforeEach
+    void setUp() {
+        this.underTest = new DefaultSkillTimerService();
+    }
+
+    @Test
+    void shouldReturnSkillTimerWhenTimerIsStarted() {
+        final var skillTimer = new SkillTimer(TIMER_AT, TimerAction.START);
+
+        final var result = underTest.startTimer(skillTimer);
+
+        StepVerifier.create(result)
+            .expectNext(skillTimer)
+            .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnEmptyWhenTimerActionDoesNotMatchWhenTimerIsStarted() {
+        final var skillTimer = new SkillTimer(TIMER_AT, TimerAction.STOP);
+
+        final var result = underTest.startTimer(skillTimer);
+
+        StepVerifier.create(result)
+            .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnSkillTimerWhenTimerIsStopped() {
+        final var skillTimer = new SkillTimer(TIMER_AT, TimerAction.STOP);
+
+        final var result = underTest.stopTimerAt(skillTimer);
+
+        StepVerifier.create(result)
+            .expectNext(skillTimer)
+            .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnEmptyWhenTimerActionDoesNotMatchWhenTimerIsStopped() {
+        final var skillTimer = new SkillTimer(TIMER_AT, TimerAction.START);
+
+        final var result = underTest.stopTimerAt(skillTimer);
+
+        StepVerifier.create(result)
+            .verifyComplete();
+    }
+}
