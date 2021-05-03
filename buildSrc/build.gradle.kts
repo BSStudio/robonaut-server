5b6bbbdd3a304c0b7b5b99ff018dafa522057c3b
csikb <22349790+csikb@users.noreply.github.com>
2021-05-03 15:32:47 +0200

    dependabot combatible gradle files

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,10 +6,7 @@ repositories {
     gradlePluginPortal()
 }
 
-extra["springBootVersion"] = "2.4.3"
-extra["springDependencyManagement"] = "1.0.11.RELEASE"
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:${property("springBootVersion")}")
-    implementation("io.spring.gradle:dependency-management-plugin:${property("springDependencyManagement")}")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.4.3")
+    implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
 }
