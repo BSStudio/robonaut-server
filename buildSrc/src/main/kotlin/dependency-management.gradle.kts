396e42f871e737e4c5003727642c69fc1f62925b
csikb <22349790+csikb@users.noreply.github.com>
2021-09-26 19:55:12 +0200

    remove unused variable

--- buildSrc/src/main/kotlin/dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/dependency-management.gradle.kts
@@ -8,12 +8,11 @@ repositories {
 }
 
 extra["springCloudVersion"] = "2020.0.4"
-extra["springDocVersion"] = "1.5.8"
 extra["mockkVersion"] = "1.12.0"
 
 dependencyManagement {
     imports {
-        // https://docs.spring.io/spring-boot/docs/2.4.5/gradle-plugin/reference/htmlsingle/#managing-dependencies-dependency-management-plugin-using-in-isolation
+        // https://docs.spring.io/spring-boot/docs/2.5.4/gradle-plugin/reference/htmlsingle/#managing-dependencies-dependency-management-plugin-using-in-isolation
         mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
     }
