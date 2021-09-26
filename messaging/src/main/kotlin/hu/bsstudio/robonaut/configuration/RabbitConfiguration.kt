6d275b541315ba9e916b29e09644dc435b4713c2
csikb <22349790+csikb@users.noreply.github.com>
2021-09-26 19:55:28 +0200

    rewrite message module

--- messaging/src/main/kotlin/hu/bsstudio/robonaut/configuration/RabbitConfiguration.kt
+++ messaging/src/main/kotlin/hu/bsstudio/robonaut/configuration/RabbitConfiguration.kt
@@ -1,62 +1,44 @@
-package hu.bsstudio.robonaut.configuration;
+package hu.bsstudio.robonaut.configuration
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionNameStrategy;
-import org.springframework.amqp.rabbit.connection.SimplePropertyValueConnectionNameStrategy;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.amqp.rabbit.connection.ConnectionNameStrategy
+import org.springframework.amqp.rabbit.connection.SimplePropertyValueConnectionNameStrategy
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-public class RabbitConfiguration {
+class RabbitConfiguration {
 
     @Bean
-    public Queue generalTeamDataQueue() {
-        return new Queue("general.teamData");
-    }
+    fun generalTeamDataQueue() = Queue("general.teamData")
 
     @Bean
-    public Queue startSkillTimerQueue() {
-        return new Queue("skill.timer");
-    }
+    fun startSkillTimerQueue() = Queue("skill.timer")
 
     @Bean
-    public Queue skillRaceGateQueue() {
-        return new Queue("skill.gate");
-    }
+    fun skillRaceGateQueue() = Queue("skill.gate")
 
     @Bean
-    public Queue safetyCarFollowQueue() {
-        return new Queue("speed.safetyCar.follow");
-    }
+    fun safetyCarFollowQueue() = Queue("speed.safetyCar.follow")
 
     @Bean
-    public Queue safetyCarOvertakeQueue() {
-        return new Queue("speed.safetyCar.overtake");
-    }
+    fun safetyCarOvertakeQueue() = Queue("speed.safetyCar.overtake")
 
     @Bean
-    public Queue startSpeedTimerQueue() {
-        return new Queue("speed.timer");
-    }
+    fun startSpeedTimerQueue() = Queue("speed.timer")
 
     @Bean
-    public Queue speedRaceLapQueue() {
-        return new Queue("speed.lap");
-    }
+    fun speedRaceLapQueue() = Queue("speed.lap")
 
     @Bean
-    public Queue teamDataQueue() {
-        return new Queue("team.teamData");
-    }
+    fun teamDataQueue() = Queue("team.teamData")
 
     @Bean
-    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+    fun jackson2JsonMessageConverter() = Jackson2JsonMessageConverter()
 
     @Bean
-    public ConnectionNameStrategy connectionNameStrategy() {
-        return new SimplePropertyValueConnectionNameStrategy("spring.application.name");
+    fun connectionNameStrategy(): ConnectionNameStrategy {
+        return SimplePropertyValueConnectionNameStrategy("spring.application.name")
     }
 }
