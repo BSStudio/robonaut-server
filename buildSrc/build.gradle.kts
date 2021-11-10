9dcaa1d2e4680ef29a77d3f8c049f2e4941403ae
csikb <22349790+csikb@users.noreply.github.com>
2021-11-10 11:55:04 +0100

    update kotlin compiler version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,11 +8,13 @@ repositories {
 
 extra["springBootVersion"] = "2.5.6"
 extra["springDependencyManagement"] = "1.0.11.RELEASE"
+extra["kotlinVersion"] = "1.6.0-RC2"
+extra["ktlintVersion"] = "10.2.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-gradle-plugin:${property("springBootVersion")}")
     implementation("io.spring.gradle:dependency-management-plugin:${property("springDependencyManagement")}")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
-    implementation("org.jetbrains.kotlin:kotlin-allopen")
-    implementation("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${property("kotlinVersion")}")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:${property("kotlinVersion")}")
+    implementation("org.jlleitschuh.gradle:ktlint-gradle:${property("ktlintVersion")}")
 }
