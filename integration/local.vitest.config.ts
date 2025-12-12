ae6ff8503f0145d18329901cc5e267e81b960a93
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-12-12 01:00:33 +0000

    fix: correct MongoDB key in application.yml configuration

--- integration/local.vitest.config.ts
+++ integration/local.vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     testTimeout: -1,
     globalSetup: 'env/local-global-setup.ts',
+    fileParallelism: false,
     maxWorkers: 1,
     isolate: false,
   },
