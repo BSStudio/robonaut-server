1ae0759ceda9075a99780c7cffce13379750d847
Bence Csik <22349790+csikb@users.noreply.github.com>
2026-01-27 08:09:28 +0000

    chore: update SDK versions and dependencies in build configuration

--- buildSrc/src/main/kotlin/hu/bsstudio/gradle/DetektConventionPlugin.kt
+++ buildSrc/src/main/kotlin/hu/bsstudio/gradle/DetektConventionPlugin.kt
@@ -1,7 +1,7 @@
 package hu.bsstudio.gradle
 
-import io.gitlab.arturbosch.detekt.DetektPlugin
-import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import dev.detekt.gradle.extensions.DetektExtension
+import dev.detekt.gradle.plugin.DetektPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
@@ -9,11 +9,10 @@ import org.gradle.kotlin.dsl.configure
 
 class DetektConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        // project.pluginManager.apply(DetektPlugin::class)
+        project.pluginManager.apply(DetektPlugin::class)
 
-        // project.extensions.configure<DetektExtension> {
-        //     buildUponDefaultConfig = true
-        //     config.setFrom(project.layout.settingsDirectory.file("detekt.yml"))
-        // }
+        project.extensions.configure<DetektExtension> {
+            config.setFrom(project.layout.settingsDirectory.file("detekt.yml"))
+        }
     }
 }
